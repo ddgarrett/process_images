3183e13d481a1d3774b6ad2ff291889b8e36fe6c
ddgarrett <doug@anetsc.com>
2023-07-14 09:05:37 -0700

    Add To Tree Right Click Menus

--- pi_tree_list.py
+++ pi_tree_list.py
@@ -8,7 +8,9 @@ import PySimpleGUI as sg
 
 import pi_config as c
 from pi_element import PiElement
+from pi_filters import SelectedTreeNodesFilter
 from pi_treedata import PiTreeData
+from status_menu_item import StatusMenuItem
 
 class PiTreeList(PiElement):
 
@@ -19,14 +21,29 @@ class PiTreeList(PiElement):
         c.listeners.add(c.EVT_TABLE_ROW_CHG,self.update_rows)
 
         menu = ['', 
-            [f'Map::{c.EVT_ACT_MAP}', 
+            ['Set Status...',[
+                 StatusMenuItem('Reject',c.STAT_REJECT,c.LVL_INITIAL,self.get_selected_rows).item(), 
+                 StatusMenuItem('Bad Quality',c.STAT_QUAL_BAD,c.LVL_QUAL,self.get_selected_rows).item(),
+                 StatusMenuItem('Duplicate',c.STAT_DUP,c.LVL_DUP,self.get_selected_rows).item(),
+                 StatusMenuItem('Just Okay',c.STAT_OK,c.LVL_OK,self.get_selected_rows).item(),
+                 StatusMenuItem('Good',c.STAT_GOOD,c.LVL_GOOD,self.get_selected_rows).item(),
+                 StatusMenuItem('Best!',c.STAT_BEST,c.LVL_BEST,self.get_selected_rows).item()],
+             'TBD - Possible...',[
+                 StatusMenuItem('Reject',c.STAT_TBD,c.LVL_INITIAL,self.get_selected_rows).item(),
+                 StatusMenuItem('Bad Quality',c.STAT_TBD,c.LVL_QUAL,self.get_selected_rows).item(),
+                 StatusMenuItem('Duplicate',c.STAT_TBD,c.LVL_DUP,self.get_selected_rows).item(),
+                 StatusMenuItem('Ok Good Best',c.STAT_TBD,c.LVL_OK,self.get_selected_rows).item(),
+                 StatusMenuItem('Good or Best',c.STAT_TBD,c.LVL_GOOD,self.get_selected_rows).item()
+                 ],
+             '---',
+             f'Map::{c.EVT_ACT_MAP}'f'Map::{c.EVT_ACT_MAP}', 
              'Properties', 
              'Review',['Initial', 'Quality','Duplicate','Selected','Best'],
              'Show',['All','Reject','Bad','Duplicate','Ok','Good','Best','Filter...'],
              'Save',
              'Exit' ]]
 
-        self._tree_data = PiTreeData(c.table)
+        self._tree_data = PiTreeData(c.table.rows())
         self._tree = (
             sg.Tree(data=self._tree_data,
                     headings=headings,
@@ -48,6 +65,23 @@ class PiTreeList(PiElement):
     def get_element(self) -> sg.Tree:
         return  self._tree
     
+    def get_selected_rows(self,values):
+        ''' Return a list of selected rows. 
+            A selected folder returns all of the rows in the folder.
+            Values is the values returned by window.read().
+            Therefore we extract the list of selected
+            files and folders via our own key.
+        '''
+        files_folders = values[self.key]
+        rows = self._tree_data.rows
+        if len(rows) == 0 or len(files_folders) == 0:
+            return []
+        
+        filter = SelectedTreeNodesFilter(files_folders)
+        rows = filter.filter(rows)
+        print(f'{len(rows)} rows selected')
+        return rows
+
     ''' Event Handlers '''
     def update_list(self,event,values):
         ''' Update the tree data after a table is loaded or filtered '''
