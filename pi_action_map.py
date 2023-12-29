12a2007102c35d25dd8f2e9fe7efb19f7a1cf61e
ddgarrett <doug@anetsc.com>
2023-12-29 14:53:33 -0800

    Generate Map Pin Description from Blog Figure Caption

--- pi_action_map.py
+++ pi_action_map.py
@@ -25,6 +25,8 @@ from my_secrets import local_api_key as apikey
     
 import webbrowser
 import gmplot
+from urllib.request import urlopen
+from urllib.error   import URLError
 
 import pi_config as c
 from pi_action import PiAction
@@ -75,6 +77,10 @@ class PiActionMap(PiAction):
 
     def _generate_map(self,rows):
         ''' generate a google maps heatmap for list of rows '''
+
+        # dictionary mapping a URI to the hmtl for that URI
+        self._page_dict = {}
+
         marker_html = "<a href='file://{c.directory}row'>The Presidio</a>"
 
         lat_lon_lst = []
@@ -91,7 +97,12 @@ class PiActionMap(PiAction):
             lat += lat_lon[0]
             lon += lat_lon[1]
             cnt += 1
-            href = f'<a href="{c.BLOG_URI}/{sub_uri}#{fn}" target="_blank">{row.get_readable_date()} - {fn}</a>'
+
+            uri = f'{c.BLOG_URI}/{sub_uri}'
+            anchor = f'{fn}'
+            href_descr = self._get_href_descr(uri,anchor)
+            href = f'<a href="{uri}#{anchor}" target="_blank">{row.get_readable_date()} - {href_descr}</a>'
+            
             try:
                 idx = lat_lon_lst.index(lat_lon)
                 weights[idx] = weights[idx] + 1
@@ -121,3 +132,88 @@ class PiActionMap(PiAction):
         gmap1.draw(map_fn)
 
         webbrowser.open_new_tab(map_fn)
+
+        # free up memory
+        self._page_dict = {}
+
+
+    def _get_href_descr(self,uri,anchor):
+        '''
+            Given the URI and anchor, return the description 
+            for the photo which follows the anchor. 
+
+            The description is based on the descrition 
+            that is enclosed by the tag:
+                <figcaption>...</figcaption>
+
+            which follows the anchor:
+                <p id="{anchor}">
+        '''
+        html = self._get_html(uri)
+        anchor_html = self._find_anchor(html,anchor)
+        return self._get_fig_caption(anchor_html,anchor)
+    
+    def _get_html(self,uri):
+        ''' Return the HTML for a given URI.
+        
+            HTML is buffered in a dictionary.
+        '''
+        if not uri in self._page_dict:
+            try:
+                with urlopen(uri) as f:
+                    # html = f.read().decode('utf-8')
+                    # TODO: do better than 'ignore'?
+                    # utf-8 decode caused errors in mapping,
+                    # such as with character: ō
+                    # Below will simply drop such characters
+                    html = f.read().decode("ASCII", 'ignore')
+
+                    ''' for debugging
+                    with open("Output.txt", "w") as text_file:
+                        text_file.write(html)
+                    '''
+            except URLError:
+                print("urlerror: ",uri)
+                html = "link not found"
+
+            self._page_dict[uri] = html
+
+        return self._page_dict[uri]
+    
+    def _find_anchor(self,html,anchor):
+        ''' Return a substring of html which 
+            follows the specified anchor
+        '''
+        tag = f'id="{anchor}"'
+        idx = html.find(tag)
+        if idx < 0:
+            print("tag not found:",tag)
+            return None
+        
+        return html[idx:]
+
+    def _get_fig_caption(self,html,default_caption):
+        ''' Find the text within the <figcaption>...</figcaption> tag
+        
+            If no figure caption, or no html, return the default caption.
+        '''
+        if not html:
+            print("err: html not found",default_caption)
+            return default_caption
+        
+        idx = html.find('<figcaption>')
+        if idx < 0:
+            print("err: caption not found",default_caption)
+            return default_caption
+        
+        html = html[idx+12:]
+        idx2 = html.find('</figcaption>')
+        if idx2 < 0:
+            print("err: end caption not found",default_caption)
+            return default_caption
+        
+        return html[0:idx2]
+        
+
+        
+
