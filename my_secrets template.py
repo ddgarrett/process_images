09191a366a8debcc066dc817a4ffd8c1d3ce0065
ddgarrett <doug@anetsc.com>
2024-03-18 08:37:15 -0700

    Prep for venv based install

---
+++ my_secrets template.py
@@ -0,0 +1,16 @@
+'''
+    To setup and configure API keys, 
+    using the gmail email user go to:
+
+      > https://console.cloud.google.com/apis/dashboard?project=????
+
+    and choose "Creditials" option on left of window
+'''
+
+# use this key for https://{user}.github.io/ html files
+# Above is update via github repo ddgarrett.github.io
+apikey = "xxx"
+
+# below must be used to replace above key if running html from
+# local file
+local_api_key = "xxx"
